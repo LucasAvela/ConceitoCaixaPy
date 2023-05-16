cf90829e87517cc05474fe809400692a1e157bfc
LucasAvela <LucasAvila08@outlook.com>
2023-05-16 17:14:41 -0300

    Add Feature: Possivel Registrar produtos à venda

---
+++ Registrar.py
@@ -0,0 +1,35 @@
+import os
+import json
+
+produtos = []
+loop = 1
+
+if os.path.exists("Produtos.json"):
+    with open("Produtos.json", "r") as json_file:
+        produtos = json.load(json_file)
+
+while loop == 1:
+
+    nome = input("Digite o Nome do produto: ")
+    cod = str(input("Escaneie o Código do produto: "))
+    valor = float(input("Entre com o Preço do produto: "))
+
+    item = {
+        "Nome": nome,
+        "Cod": cod,
+        "Valor": valor
+    }
+
+    produtos.append(item)
+
+    Input = int(input(" \n1.Continuar\n2.Finalizar\n: "))
+
+    if Input == 1:
+        loop = 1
+    else:
+        loop = 0
+
+json_data = json.dumps(produtos, indent=4)
+
+with open("./Produtos.json", "w") as json_file:
+    json_file.write(json_data)
